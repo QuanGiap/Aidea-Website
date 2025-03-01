bdff4325ff92553b66b42142fb1d4cebb952bfa9
Quan Giap <minhquangiapnguyen@gmail.com>
2025-03-01 10:32:55 -0800

    Create project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Route, Routes } from 'react-router'
+import Home from './Page/Home/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Routes>
+      <Route index element={<Home />}/>
+    </Routes>
+  )
+}
+
+export default App
